6accbc9cd40e8ee1ad4c1d7cb3d3390af0e6d748
neal.shen <neal.shen@aliyun.com>
2022-03-03 13:44:11 +0800

    V3.4.14 for test

--- Include/DCAM550/Vzense_api_550.h
+++ Include/DCAM550/Vzense_api_550.h
@@ -591,4 +591,11 @@ VZENSE_C_API_EXPORT PsReturnStatus Ps2_GetDeviceMAC(PsDeviceHandle device, uint3
 */
 VZENSE_C_API_EXPORT PsReturnStatus Ps2_RebootCamera(PsDeviceHandle device, uint32_t sessionIndex);
 
+/**
+* @brief		Enables or disables the legacy algorithmic,and default value is disabled.
+* @param[in]	device			The handle of the device on which to enable or disable the feature.
+* @param[in] 	sessionIndex	The index of the session. See ::Ps2_StartStream() & ::Ps2_StopStream() api for more information.
+* @return 		::PsRetOK		if the function succeeded, or one of the error values defined by ::PsReturnStatus.
+*/
+VZENSE_C_API_EXPORT PsReturnStatus Ps2_SetLegacyAlgorithmicEnabled(PsDeviceHandle device, uint32_t sessionIndex, bool enabled);
 #endif /* VZENSE_API_550_H */
